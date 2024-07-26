94a98f9908fff2acdc645162dc8204e6d6b0ce26
Matthew Hayward <matthewh86@users.noreply.github.com>
2024-07-26 11:47:28 +0100

    Update SCP EXILED and plugins

--- scpsl-exiled/download.sh
+++ scpsl-exiled/download.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 # https://github.com/Exiled-Team/EXILED/releases
-curl -LO 'https://github.com/Exiled-Team/EXILED/releases/download/5.2.2/Exiled.tar.gz'
+curl -LO 'https://github.com/Exiled-Team/EXILED/releases/download/8.9.11/Exiled.tar.gz'
 
 # https://github.com/Exiled-Team/Common-Utils
- curl -LO 'https://github.com/Exiled-Team/Common-Utils/releases/download/6.2.0/Common_Utilities.dll'
+ curl -LO 'https://github.com/Exiled-Team/Common-Utils/releases/download/7.1.1/Common.Utilities.dll'
 
 # https://github.com/Michal78900/RespawnTimer
- curl -LO 'https://github.com/Michal78900/RespawnTimer/releases/download/v3.1.0/RespawnTimer.dll'
-
-# https://github.com/VersLugia/ClassBroadcastReborn
- curl -LO 'https://github.com/VersLugia/ClassBroadcastReborn/releases/download/1.1/ClassBroadcastReborn.dll'
+ curl -LO 'https://github.com/Michal78900/RespawnTimer/releases/download/v4.0.4/RespawnTimer.dll'
 
 # https://github.com/babyboucher/RPNames
- curl -LO 'https://github.com/babyboucher/RPNames/releases/download/v1.8.0/RPNames.dll'
+ curl -LO 'https://github.com/1EnesBaturKaza/RPNames/releases/download/v2.2.0/RPNames.dll'
 
 # https://github.com/DGvagabond/SCP-008-X
  curl -LO 'https://github.com/DGvagabond/SCP-008-X/releases/download/3.1.2/SCP008X.dll'
