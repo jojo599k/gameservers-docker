1acdcd305097b9c0a817aa3d3f87e88983554bbb
Matthew Hayward <matthewh86@gmail.com>
2024-08-05 22:44:27 +0100

    conform css to new pattern, metamod and gungame

--- css-gungame/download.sh
+++ css-gungame/download.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-# [CS:S/CS:GO] GunGame
-# https://forums.alliedmods.net/showthread.php?t=93977
 curl -o sm_gungame.zip 'https://forums.alliedmods.net/attachment.php?attachmentid=133712&d=1400696532'
+curl -o sm_ggdm-1.8.0.zip 'https://forums.alliedmods.net/attachment.php?attachmentid=108943&d=1346584450'
+unzip sm_gungame.zip
+unzip sm_ggdm-1.8.0.zip
+
+curl -o bot_endround.smx 'https://www.sourcemod.net/vbcompiler.php?file_id=45192'
+mv bot_endround.smx addons/sourcemod/plugins/bot_endround.smx
