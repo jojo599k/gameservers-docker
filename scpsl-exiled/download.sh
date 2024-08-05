d2016beb2f3df34edc31945b26bd2193d86b5d94
Matthew Hayward <matthewh86@gmail.com>
2024-08-05 22:43:08 +0100

    update scpsl-exiled plugins

--- scpsl-exiled/download.sh
+++ scpsl-exiled/download.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+set -e
+
 # https://github.com/Exiled-Team/EXILED/releases
+
 curl -LO 'https://github.com/Exiled-Team/EXILED/releases/download/8.9.11/Exiled.tar.gz'
+tar -xzvf Exiled.tar.gz
+cp -r EXILED "${CONFIG_DIR}"
+rm -fr EXILED
 
 # https://github.com/Exiled-Team/Common-Utils
  curl -LO 'https://github.com/Exiled-Team/Common-Utils/releases/download/7.1.1/Common.Utilities.dll'
@@ -13,4 +19,7 @@ curl -LO 'https://github.com/Exiled-Team/EXILED/releases/download/8.9.11/Exiled.
  curl -LO 'https://github.com/1EnesBaturKaza/RPNames/releases/download/v2.2.0/RPNames.dll'
 
 # https://github.com/DGvagabond/SCP-008-X
- curl -LO 'https://github.com/DGvagabond/SCP-008-X/releases/download/3.1.2/SCP008X.dll'
+curl -LO 'https://github.com/DGvagabond/SCP-008-X/releases/download/3.1.2/SCP008X.dll'
+
+mkdir -p "${CONFIG_DIR}/EXILED/Plugins/"
+mv *.dll "${CONFIG_DIR}/EXILED/Plugins/"
