79fc6ebb23f4943cd66a871dafc621ce195d039c
Matthew Hayward <matthewh86@gmail.com>
2024-08-08 14:03:10 +0100

    tidy up l4d2 filenames

--- l4d2/start.sh
+++ l4d2/start.sh
@@ -9,6 +9,6 @@ set -e
 [[ -z $MAXPLAYERS ]] && MAXPLAYERS="8"
 [[ -z $MP_GAMEMODE ]] && MP_GAMEMODE="versus"
 
-./update_server_details.sh
+./configure.sh
 
 exec ./srcds_run -game left4dead2 +sv_lan $LAN +ip $SERVER_IP +map $MAP +mp_gamemode $MP_GAMEMODE -usercon -port 27015 -maxplayers $MAXPLAYERS -secure $OTHER_ARGS $@
