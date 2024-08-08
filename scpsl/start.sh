082f6ade1b2eacc7be816f9c8b81a9db65b4af0e
Matthew Hayward <matthewh86@gmail.com>
2024-08-08 14:01:11 +0100

    add EulaAccepted config file

--- scpsl/start.sh
+++ scpsl/start.sh
@@ -9,4 +9,10 @@ mkdir -p "${CONFIG_DIR}" \
   && cp -r config_gameplay.txt "${CONFIG_DIR}/config_gameplay.txt" \
   && cp -r config_remoteadmin.txt "${CONFIG_DIR}/config_remoteadmin.txt"
 
+DATE_ACCEPTED=$(date -u +"%Y-%m-%dT%H:%M:%S.%NZ")
+
+cat <<EOF >/home/steam/.config/SCP\ Secret\ Laboratory/config/localadmin_internal_data.json
+{"GitHubPersonalAccessToken":null,"EulaAccepted":"${DATE_ACCEPTED}","PluginManagerWarningDismissed":false,"LastPluginAliasesRefresh":null,"PluginVersionCache":{},"PluginAliases":{}}
+EOF
+
 ./LocalAdmin ${SERVER_PORT} -d
